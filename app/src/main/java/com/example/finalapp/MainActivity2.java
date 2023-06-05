67f58aeeb948d7ea54ba70413aaea35fd322a9f8
junaid786 <junaidchaudahry@gmail.com>
2023-06-05 22:36:03 +0500

    this is my second commit

--- app/src/main/java/com/example/finalapp/MainActivity2.java
+++ app/src/main/java/com/example/finalapp/MainActivity2.java
@@ -52,7 +52,7 @@ public class MainActivity2 extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                listView.setAdapter(arrayAdapter);
             }
         });
     }
